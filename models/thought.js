15a900251867a78cc056cf5f861552cb5e0f8db4
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-04-03 21:31:57 -0400

    deleted reaction model and added it to thought model as a subdoc

--- models/thought.js
+++ models/thought.js
@@ -1,7 +1,7 @@
 const { Schema } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const thoughtSchema = new Schema (
+const ThoughtSchema = new Schema (
   {
     thoughtText:{
       type: String,
@@ -14,15 +14,12 @@ const thoughtSchema = new Schema (
       default: Date.now,
       get: (createdAtVal) => dateFormat(createdAtVal),
     },
-    // need to connect user that created this thought
     username: {
       type:String,
       required: true,
+      ref: 'User'
     },
-    // like replies
-    reactions:{
-
-    },
+    reactions: [ReactionSchema],
   },
   {
     toJSON: {
@@ -30,6 +27,34 @@ const thoughtSchema = new Schema (
     },
   }
 );
+
+const ReactionSchema = new Schema (
+  {
+    reactionId:{
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    createdAt:{
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+      type:String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    }
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reaction.length;
+});
+
 
 const Thought = module('Thought', thoughtSchema)
 
